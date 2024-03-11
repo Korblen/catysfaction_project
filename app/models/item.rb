5207fdc926657220141897ed9ad69d8339734d38
Ton nom d'utilisateur githubYannRZG <yann.rezigui@outlook.fr>
2024-03-11 14:54:07 +0100

    validates/bootstrap

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,9 @@ class Item < ApplicationRecord
   # table N-N through
   has_many :order_items
   has_many :orders, through: :order_items
+
+  validates :title, presence: true, length: { in: 5..140 }
+  validates :description, presence: true, length: { in: 20..1000 }
+  validates :price,  presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 1000 }
+  
 end
